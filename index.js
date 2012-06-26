fbf3b1df00bcc71f210d914fcfa03a8a18cc66d5
TJ Holowaychuk <tj@vision-media.ca>
2012-06-26 11:28:42 -0700

    move Test to lib/test.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * Module dependencies.
  */
 
-var request = require('superagent')
-  , methods = require('./lib/methods')
-  , Request = request.Request
+var methods = require('./lib/methods')
+  , Test = require('./lib/test')
   , http = require('http');
 
 module.exports = function(app){
@@ -25,44 +24,3 @@ module.exports = function(app){
 
   return obj;
 };
-
-/**
- * Initialize a new `Test` with the given `app`,
- * request `method` and `path`.
- *
- * @param {Server} app
- * @param {String} method
- * @param {String} path
- * @api public
- */
-
-function Test(app, method, path) {
-  Request.call(this, method, path);
-  this.app = app;
-}
-
-/**
- * Inherits from `Request.prototype`.
- */
-
-Test.prototype.__proto__ = Request.prototype;
-
-/**
- * Defer invoking superagent's `.end()` until
- * the server is listening.
- *
- * @param {Function} fn
- * @api public
- */
-
-Test.prototype.end = function(fn){
-  var self = this;
-  var end = Request.prototype.end;
-  var app = this.app;
-  var addr = app.address();
-  if (!addr) return app.listen(0, function(){ self.end(fn); });
-  this.url = 'http://' + addr.address + ':' + addr.port + this.url;
-  end.call(this, fn);
-};
-
-
