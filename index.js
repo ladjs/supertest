58d1611b61d6964ec4956450996e7d6fdd9c31c7
TJ Holowaychuk <tj@vision-media.ca>
2012-06-26 11:32:10 -0700

    docs

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var methods = require('./lib/methods')
   , Test = require('./lib/test')
   , http = require('http');
 
+/**
+ * Test against the given `app`,
+ * returning a new `Test`.
+ *
+ * @param {Function|Server} app
+ * @return {Test}
+ * @api public
+ */
+
 module.exports = function(app){
   if ('function' == typeof app) app = http.createServer(app);
   var obj = {};
